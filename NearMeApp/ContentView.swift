bad8d24d8dc1150ad70de6997df811cb69c53264
Brian Surface <brian.surface@outlook.com>
2024-10-05 11:28:23 -0400

    Initial Commit

---
+++ NearMeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NearMeApp
+//
+//  Created by Brian Surface on 10/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NearMeApp/NearMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeAppApp.swift
+//  NearMeApp
+//
+//  Created by Brian Surface on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct NearMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
